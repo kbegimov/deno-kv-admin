bb2f1137835e1660d00d52fb637ba7fb565ca88c
kbegimov <kbegimov@gmail.com>
2026-02-14 18:40:36 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -131,7 +131,7 @@ app.onError((err, c) => {
 // Insure security - The autograder will have you change this value
 function checkToken(c) {
   const token = c.req.query("token");
-  if ( token == '42' ) return true;
+  if ( token == '2605_98671a:f9093a' ) return true;
   throw new HTTPException(401, { message: 'Missing or invalid token' }); 
 }
 
